8fe2d406bcfe75a1170889996056ed3751397807
Kiana Nezafat Yazdi <kiananezafat6.kn@gmail.com>
2025-01-31 09:18:48 -0500

    Question 7

--- Week4/lab_week4.py
+++ Week4/lab_week4.py
@@ -170,6 +170,11 @@ loot = loot_options.pop(lootRoll-1)
 belt.append(loot)
 print("Your belt: " , belt)
 
+#Lab 04 - Q7 - Sort the belt
+print("you got the loots, so organize your belt alphabetically")
+belt.sort()
+print("Your belt: " , belt)
+
 input("Analyze the roll (Press enter)")
 # Compare Player vs Monster's strength
 print("--- You are matched in strength: " + str(combat_strength == m_combat_strength))
