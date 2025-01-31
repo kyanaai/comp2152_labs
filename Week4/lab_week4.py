8cabe533b5f9f9674456085ecc8e934265a7adda
Kiana Nezafat Yazdi <kiananezafat6.kn@gmail.com>
2025-01-31 09:16:05 -0500

    Question 5&6

--- Week4/lab_week4.py
+++ Week4/lab_week4.py
@@ -155,6 +155,21 @@ input("Roll the dice for the monster's health points (Press enter)")
 m_health_points = random.choice(diceOptions)
 print("Player rolled " + str(m_health_points) + " health points for the monster")
 
+#Lab 04 - Q5
+print("You find a loot bag! Look inside to find 2 items")
+input("Roll for the first item(Press enter)")
+lootRoll=random.choice(range(1, len(loot_options)+1))
+loot = loot_options.pop(lootRoll-1)
+belt.append(loot)
+print("Your belt: " , belt)
+
+#lab 04 - Q6 - Item 2
+input("Roll for the second item(Press enter)")
+lootRoll=random.choice(range(1, len(loot_options)+1))
+loot = loot_options.pop(lootRoll-1)
+belt.append(loot)
+print("Your belt: " , belt)
+
 input("Analyze the roll (Press enter)")
 # Compare Player vs Monster's strength
 print("--- You are matched in strength: " + str(combat_strength == m_combat_strength))
