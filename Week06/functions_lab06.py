38d95a02f9f63e8ea75b53e0098abdc4cdae470c
Kiana Nezafat Yazdi <kiananezafat6.kn@gmail.com>
2025-02-14 09:35:13 -0500

    Lab06

--- Week06/functions_lab06.py
+++ Week06/functions_lab06.py
@@ -159,3 +159,17 @@ def load_game():
         return None
 
 # Lab 06 - Question 5b
+def adjust_combat_strength(combat_strength , m_combat_strength):
+    last_game = load_game()
+    if last_game:
+        if "Hero" in last_game and "gained" in last_game:
+            num_stars = int(last_game.split()[-2])
+            if num_stars > 3 :
+                print("    |    Increasing the monster's combat strength")
+                m_combat_strength += 1
+        elif "Monster killed the" in last_game:
+            print("    |    Increasing the hero's combat strength")
+            combat_strength += 1
+        else:
+            print("    |    ... last game had no effect on Hero/Monster combat strength")
+    
--- Week06/lab06.py
+++ Week06/lab06.py
@@ -6,8 +6,10 @@ import functions_lab06
 
 print("    ------------------------------------------------------------------")
 print("    |    Loading previous game data...")
+
 # Lab 06 - Question 5
-functions_lab06.load_game()
+last_game = functions_lab06.load_game()
+
 # Game Flow
 # Define two Dice
 small_dice_options = list(range(1, 7))
@@ -88,6 +90,7 @@ if not input_invalid:
     print("    |    The hero\'s weapon is " + str(weapons[weapon_roll - 1]))
 
     # Lab 06 - Question 5b
+    functions_lab06.adjust_combat_strength(combat_strength , m_combat_strength)
     
     # Weapon Roll Analysis
     print("    ------------------------------------------------------------------")
@@ -175,9 +178,27 @@ if not input_invalid:
         m_combat_strength) + " using the " + power_roll + " magic power")
     # Lab 06 - Question 6
 
+
     # Call Recursive function
-    print("    |", end="    ")
-    num_dream_lvls = input("How many dream levels do you want to go down?")
+    num_dream_lvls = -1
+    while (num_dream_lvls <0 or num_dream_lvls > 3):
+        print("    |", end="    ")
+        num_dream_lvls = input("How many dream levels do you want to go down?")
+        if(num_dream_lvls==""):
+            num_dream_lvls = -1
+            print("Dream levels should be a whole number between 0 and 3 , try again")
+        else:
+            num_dream_lvls = int(num_dream_lvls)
+            if(num_dream_lvls <0 or num_dream_lvls > 3):
+                num_dream_lvls = -1
+                print("Dream levels should be a whole number between 0 and 3 , try again")
+            elif (not num_dream_lvls ==0 ):
+                health_points -= 1
+                crazy_level = functions_lab06.inception_dream(num_dream_lvls)
+                combat_strength += crazy_level
+                print("combat strength: " + str(combat_strength))
+                print("health points: " + str(health_points))
+        print("num_dream_lvls: " , num_dream_lvls)
     if num_dream_lvls != 0:
         health_points -= 1
         crazy_level = functions_lab06.inception_dream(num_dream_lvls)
