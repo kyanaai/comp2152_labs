ea7ee43ac4c721ed450c75cd59975ed39f5409ba
Kiana Nezafat Yazdi <kiananezafat6.kn@gmail.com>
2025-03-14 08:48:28 -0400

    Week 10 main

--- Week10/main.py
+++ Week10/main.py
@@ -2,3 +2,29 @@ import sqlite3
 from contextlib import closing
 
 db_path="sqlite3.db"
+
+try:
+    with closing(sqlite3.connect(db_path)) as db_conn:
+        db_conn.row_factory = sqlite3.Row
+        with closing(db_conn.cursor()) as cursor:
+            try:
+                queru_1 = "SELECT * FROM demo WHRER id > 14"
+                cursor.execute(queru_1)
+                rows = cursor.fetchall()
+                print("Name of rows with id > 14:\n")
+                for row in rows:
+                    print(row["name"])
+            except Exception as e:
+                print(F"Error executing query_1: {e} ")
+            #delete row based on the user input
+            try:
+                del_row= int(input("Enter the row ID threshold for deletion: "))
+                query_2 = "DELETE FROM demo WHERE id < ?"
+                cursor.execute(query_2, (del_row))
+                num_rows = cursor.rowcount
+                print(f"{num_rows} rows affected.")
+                db_conn.commit()
+            except Exception as e:
+                print(F"Error executing query_2: {e} ")
+except sqlite3.Error as e:
+    print(f"Database Connection Error: {e}")
