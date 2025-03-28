a73b8636805a1db7cbe1add6053a296753bbbcfc
Kiana Nezafat Yazdi <kiananezafat6.kn@gmail.com>
2025-03-28 08:51:18 -0400

    Final Edition

--- Week12/person.py
+++ Week12/person.py
@@ -10,19 +10,27 @@ class Person(Mammal):
     # __height
 
     def __init__(self, p_name, p_age, p_height):
+        Mammal.__init__(self, p_age)
         # Lab 12 - Call the parent class constructor
 
         # Set the Person-specific fields
         print("Constructor: Adding the Person parts of a person")
-
+        self.name = p_name
+        #self.age = p_age
+        self.__height = p_height
 
     def __del__(self):
         print("Destructor: The garbage collector is now deleting the person object")
-        
+        super(Person, self).__del__()
 
         # Complex getter for height
-
+    @property
+    def height(self):
+        return self.__height
     # Complex setter for height
+    @height.setter
+    def height(self, p_height):
+        self.__height = p_height
 
 
     # Lab 12 - Private or protected METHODS are not possible in python.
